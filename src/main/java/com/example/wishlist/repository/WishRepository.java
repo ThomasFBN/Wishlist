a24bab7d48b89667b90a4d1220bed6a559e609ac
OliverLyksted <144331612+OliverLyksted@users.noreply.github.com>
2024-04-09 13:27:51 +0200

    commit

--- src/main/java/com/example/wishlist/repository/WishRepository.java
+++ src/main/java/com/example/wishlist/repository/WishRepository.java
@@ -1,8 +1,9 @@
 package com.example.wishlist.repository;
 
+import com.example.wishlist.model.Wishlist;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class WishRepository {
-
+    private Wishlist wishlist;
 }
