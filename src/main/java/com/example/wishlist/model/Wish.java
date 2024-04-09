397f476334a8a60f28eaee6a738a8ea5caeae30f
OliverLyksted <144331612+OliverLyksted@users.noreply.github.com>
2024-04-09 13:26:50 +0200

    små ting

--- src/main/java/com/example/wishlist/model/Wish.java
+++ src/main/java/com/example/wishlist/model/Wish.java
@@ -1,4 +1,15 @@
 package com.example.wishlist.model;
 
 public class Wish {
+
+    private String name;
+    private String description;
+
+
+    public String getDescription() {
+        return description;
+    }
+    public String getName() {
+        return name;
+    }
 }
