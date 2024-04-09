87fdb162796e139995c72e5010d22d8eb0154841
ThomasFBN <thni0004@stud.kea.dk>
2024-04-09 13:07:54 +0200

    Packages + klasser

---
+++ src/main/java/com/example/wishlist/controller/WishController.java
@@ -0,0 +1,28 @@
+package com.example.wishlist.controller;
+
+import com.example.wishlist.model.Wishlist;
+import com.example.wishlist.service.WishService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class WishController {
+    private WishService wishService;
+    public WishController(WishService wishService){
+        this.wishService = wishService;
+    }
+    @GetMapping("/home")
+    public String getHomePage(){
+        return "WishList";
+    }
+    @GetMapping("/create")
+    public String createWishList() {
+
+        return "CreateWishList";
+    }
+
+
+
+}
---
+++ src/main/java/com/example/wishlist/repository/WishRepository.java
@@ -0,0 +1,8 @@
+package com.example.wishlist.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class WishRepository {
+
+}
